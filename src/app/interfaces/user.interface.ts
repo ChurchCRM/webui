99a85c17c686ce785dbd9fea9fe6eb64229c0d7f
DiogoFilhoDeMorais <analise.federal@gmail.com>
2023-10-27 18:24:57 -0300

    Changing from Angular to React

--- src/app/interfaces/user.interface.ts
+++
@@ -1,8 +0,0 @@
-export interface User {
-  created_at?: string | null,
-  email?: string | null,
-  name?: string | null,
-  token?: string | null,
-  uf?: string | null,
-  updated_at?: string | null,
-}
--- src/app/ngrx/router/router.selectors.ts
+++
@@ -1,8 +0,0 @@
-import { createSelector } from '@ngrx/store';
-
-export const routerSelector = (router: any) => router;
-
-export const router = createSelector(
-  routerSelector,
-  (ngrx) => (ngrx.router.state.url)
-);
